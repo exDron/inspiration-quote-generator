46dcfa05a055d83d6f929cd677ab82902f2b61cf
Andrii Tkachuk <andrey.tkachuk89@gmail.com>
2023-11-04 11:35:23 +0100

    Added gradient backgroud container

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElemets'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inspirational Quote Generator</title>
+      </Head>
+      <GradientBackgroundCon></GradientBackgroundCon>
+    </>
+  )
+}
